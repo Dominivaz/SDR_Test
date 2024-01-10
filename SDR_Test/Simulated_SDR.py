94bbdace421ff74ef445f8bf6b3fa684b2ec2545
Dominivaz <dominiv@berkeley.edu>
2024-01-10 14:56:26 -0800

    fixed

--- SDR_Test/Simulated_SDR.py
+++ SDR_Test/Simulated_SDR.py
@@ -8,14 +8,8 @@ class SDR:
         
     def data(self):
         ntimes = np.linspace(0, 1, 12288)
-#         self.tone = tone
-#         self.freq = freq
-#         self.time = np.arange(ntimes)/self.sample_freq
         self.sin = np.sin(2*np.pi*self.sample_freq*ntimes)
         return self.sin
-#         sample = int(np.round(sample_freq/sample_rate))
-#         print(sample)
-#         return sin[::sample]
     
     def capture(self):
         sample = int(np.round(self.sample_freq/self.sample_rate))
@@ -23,14 +17,3 @@ class SDR:
         x = self.data()
         print(x.shape)
         return x[::sample]
-    
-#     def sample(self, freq):
-#         sampling_freq = signal_freq/2
-#         data = wave[::6]
-#         return
-        
-n1 = SDR(28.8e6, 3.2e6)
-print(n1.sample_freq)
-print(n1.sample_rate)
-n1.hello()
-n1.capture()
