03fe4b8a55d1b7ff1a39b3f48a2820d443945458
Dominivaz <dominiv@berkeley.edu>
2024-04-23 11:32:18 -0700

    updated script added

---
+++ SDR_Test/scripts/phaselock_threshold_clean.py
@@ -0,0 +1,139 @@
+import numpy as np
+import ugradio
+import SDR_Test
+import datetime
+import time
+import board
+import busio
+import adafruit_mcp4725
+import os
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+
+def files(file_number):
+    os.mkdir('/home/radiopi/sync_data/sdr/threshold/dac_values' + file_number)
+    os.mkdir('/home/radiopi/sync_data/sdr/threshold/d_f_values' + file_number)
+    os.mkdir('/home/radiopi/sync_data/sdr/threshold/wave_values' + file_number)
+    os.mkdir('/home/radiopi/sync_data/sdr/threshold/unix_values' + file_number)
+    
+def sampling(nsamples, nblocks, tone, start, stop):
+    data = sdr.capture_data(nsamples=nsamples,nblocks=nblocks)
+    data = np.concatenate(data)
+    amount = data.size
+    df = SDR_Test.receive_sync_test30.receive_fitting(data, amount, start, stop, f_sample = sdr.get_sample_rate(), tone = tone)
+    start += amount
+    stop += amount
+    return df
+
+def DAC_change(diff):
+    changing = SDR_Test.receive_sync_test30.receive_sync(diff)
+    dac.raw_value += int(changing)
+    
+def appending():
+    dac_values.append(dac.raw_value)
+    wave_values.append(data)
+    df_values.append(offset)
+    unix_values.append(lock_time)
+    
+def save():
+    current_time = datetime.datetime.now()
+    timestamp = current_time.strftime('%Y-%m-%d_%H-%M-%S')
+        
+    np.save('/home/radiopi/sync_data/sdr/threshold/dac_values' + file_number + f'/dac_values_{timestamp}', np.array(dac_values))
+    np.save('/home/radiopi/sync_data/sdr/threshold/d_f_values' + file_number + f'/d_f_values_{timestamp}', np.array(df_values))
+    np.save('/home/radiopi/sync_data/sdr/threshold/wave_values' + file_number + f'/wave_values{timestamp}', np.array(wave_values))
+    np.save('/home/radiopi/sync_data/sdr/threshold/unix_values' + file_number + f'/unix_values_{timestamp}', np.array(unix_values))
+    print(f'saved on {timestamp}')    
+    print('saved!')
+    
+def reset():
+    dac_values = []
+    wave_values = []
+    df_values = []
+    unix_values = []
+
+    
+parser = ArgumentParser(description = 'files', formatter_class = ArgumentDefaultsHelpFormatter)
+
+parser.add_argument('-f', dest = 'file_number_', type = int, help = 'File number for data')
+parser.add_argument('-n', dest = 'fs_number_', type = float, default = 2.2, help = 'Sampling frequency for SDR in MHz')
+parser.add_argument('-s', dest = 'sample_number_', type = int, default = 8192, help = 'nsamples for SDR')
+parser.add_argument('-b', dest = 'block_number_', type = int, default = 20, help = 'nblocks for SDR')
+parser.add_argument('-t', dest = 'tone_number_', type = float, default = .2, help = 'Frequency being sampled in MHz')
+parser.add_argument('-p', dest = 'phase_number_', type = float, default = 2, help = 'Max and min phase difference allowed before syncing in Hz')
+
+args = parser.parse_args()
+file_number = args.file_number_
+fs_number = args.fs_number_ * 1e6
+sample_number = args.sample_number_
+block_number = args.block_number_
+tone_number = args.tone_number_ * 1e6
+file_number = str(file_number) + '/'
+phase_number = args.phase_number_
+
+
+
+files(file_number)
+blocks = block_number
+tone = tone_number
+fs = fs_number
+samples = sample_number
+beginning = 0
+end = samples*blocks
+threshold = phase_number
+
+file_counter = 0
+num = 0
+limit = 100
+
+i2c = busio.I2C(board.SCL, board.SDA)
+dac = adafruit_mcp4725.MCP4725(i2c)
+dac.raw_value = 0
+sdr = ugradio.sdr.SDR(sample_rate = fs, direct=True)
+
+dac_values = []
+wave_values = []
+df_values = []
+unix_values = []
+
+###
+
+offset = 10
+
+while offset > .4:
+    print('Locking')
+    offset = sampling(samples,blocks,tone,beginning,end)
+    DAC_change(offset)
+    lock_time = time.time()
+    appending()
+    num += 1
+    print(num)
+    if num == limit:
+        save()
+        reset()
+        num = 0
+        file_counter += 1
+        print('Number of times saved:', file_counter)
+
+
+###
+print('Locking ended, waiting for '+ str(threshold) +' phase difference...')
+
+start_time = time.time()
+end_time = time.time()
+
+while True:
+    df = sampling(samples,blocks,tone,beginning,end)
+    if abs(df) > threshold:
+        DAC_change(df)
+        start_time = time.time()
+        print('Adjusted DAC')
+    end_time = time.time()
+    appending()
+    num += 1
+    print(num)
+    if num == limit:
+        save()
+        reset()
+        num = 0
+        file_counter += 1
+        print('Number of times saved:', file_counter)
